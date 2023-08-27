b2db71dff843062db72ffbfe0f1342e78c511d57
Anton Bogdanov <xawek@yandex.ru>
2023-08-28 01:50:20 +0500

    Fixes after review

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Ingredient, Recipe, Tag, Favorite, ShoppingCart
+from .models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
 
 
 @admin.register(Tag)
